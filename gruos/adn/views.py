02c11e8b75bee18ef3576e64a643a3942d05abbe
MarioCodanai <85038810+MarioCodenai@users.noreply.github.com>
2021-11-04 12:19:58 -0700

    master: Se cambia el readme y agregan excepeciones

--- gruos/adn/views.py
+++ gruos/adn/views.py
@@ -32,23 +32,31 @@ class DnaView(APIView):
             serializer = DnaSeralizer(data=data_insert)
             if serializer.is_valid():
                 serializer.save()
-                
+            else:
+               return Response({"message" : "Bad request"},status=status.HTTP_400_BAD_REQUEST) 
+              
             return Response({"message" : "DNA has a mutation"},status=status.HTTP_200_OK)
         
         data_insert = {"dna":data['dna'],"mutation":False}
         serializer = DnaSeralizer(data=data_insert)   
         if serializer.is_valid():
             serializer.save()
+        else:
+            return Response({"message" : "Bad request"},status=status.HTTP_400_BAD_REQUEST)
                 
         return Response({"message" : "DNA has no mutation"},status=status.HTTP_403_FORBIDDEN)
     
 class StatsView(APIView):
     
     def get(self,request):
-        count_mutations = len(DnaRegistration.objects.filter(mutation=True))
-        count_no_mutation = len(DnaRegistration.objects.filter(mutation=False))
-        ratio = count_mutations / count_no_mutation
-        data = {"count_mutations":count_mutations,"count_no_mutation":count_no_mutation,"ratio":ratio}
-        return Response(data)
+        
+        try:
+            count_mutations = len(DnaRegistration.objects.filter(mutation=True))
+            count_no_mutation = len(DnaRegistration.objects.filter(mutation=False))
+            ratio = count_mutations / count_no_mutation
+            data = {"count_mutations":count_mutations,"count_no_mutation":count_no_mutation,"ratio":ratio}
+            return Response(data,status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"message" : "Bad request"},status=status.HTTP_400_BAD_REQUEST)
     
         
